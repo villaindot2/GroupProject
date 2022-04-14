71cad71b1fe477254c8b2edfdd029e27fca83688
villaindot2 <dankokodundundof@gmail.com>
2022-04-14 22:11:08 +0600

    Content Dana

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import * as React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './components/Header';
+import Content from './components/Content';
 
 const App = () => {
   return (
     <>
-    
     <Header />
+    <Content />
     
     
     </>
